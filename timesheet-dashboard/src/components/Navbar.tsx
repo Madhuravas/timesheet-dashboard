66bb226e391e2c4ba88c6bc0e271911b02237173
Madhuravas <madhuravasreddy1998@gmail.com>
2025-10-02 19:22:53 +0530

    Created react project and added login page and navbar components

---
+++ timesheet-dashboard/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+        <nav className="bg-blue-600 p-4 text-white sticky w-full top-0 left-0">
+            <h1 className="text-2xl font-bold">Navbar</h1>
+        </nav>
+    )
+};
+
+export default Navbar;
---
+++ timesheet-dashboard/src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Login from "../components/Login";
+
+function Main() {
+    return (
+        <>
+            {/* <Navbar /> */}
+            <Login />
+        </>
+    )
+};
+
+export default Main;
